874d830d60ce7df48e017cd186c9438a719b8c82
Selectel Git User <git-user@selectel.ru>
2023-09-05 01:01:04 +0600

    Task4 last

---
+++ Seminar1Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine()??"0");
+
+Console.WriteLine(num < 1000 && num > 99 ? num % 10 : "Число не трёхзначное");
+//последняя цифра числа
+
+//Console.WriteLine("Введите число:");
+//int num = int.Parse(Console.ReadLine()??"0");
+
+//Console.WriteLine(num % 10);
