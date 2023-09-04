918d49f79a61067e1ab136ffd298e840a8086c57
Selectel Git User <git-user@selectel.ru>
2023-09-05 00:49:06 +0600

    таски0,1,2,3

---
+++ Seminar1Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите два числа");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+int numb2 = Convert.ToInt32(Console.ReadLine());
+int x = numb2 * numb2;
+if (numb1 == x) 
+{
+    System.Console.WriteLine("Первое число является квадратом второго");
+}
+else 
+{
+ System.Console.WriteLine("первое число не является квадратом второго");
+}
