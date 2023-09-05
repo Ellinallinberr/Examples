ea3a78c23f08572995044af82c8798c8a19a8c49
Selectel Git User <git-user@selectel.ru>
2023-09-05 18:20:33 +0600

    Example006_Base/

--- Example006_Base/Program.cs
+++ Example006_Base/Program.cs
@@ -4,13 +4,13 @@ int c = 3;
 int d = 8;
 int e = 5;
 
-int max = a
+int max = a;
 
-if(a>max) max = a;
-if(b>max) max = b;
-if(c>max) max = c;
-if(d>max) max = d;
-if(e>max) max = e;
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = e;
 
-Console.Write("max= ");
+Console.Write("max = ");
 Console.WriteLine(max);
