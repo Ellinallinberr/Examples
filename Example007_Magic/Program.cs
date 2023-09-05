4aa7768576b40a2dd715b5de085fb86d22b1ed7a
Selectel Git User <git-user@selectel.ru>
2023-09-05 19:45:40 +0600

    Example007_Magic/

---
+++ Example007_Magic/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
+int xa = 1;
+int ya = 1;
+int xb = 1;
+int yb = 30;
+int xc = 80;
+int yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
