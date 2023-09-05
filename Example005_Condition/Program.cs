71e25a0b04cf2161cb8499da2befc7c5195c4d0a
Selectel Git User <git-user@selectel.ru>
2023-09-05 18:11:30 +0600

    Example005_Condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();//??"0"
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! это же МАША!");
+     
+}
+else
+{
+   Console.Write("Привет, ");
+   Console.WriteLine(username); 
+}
