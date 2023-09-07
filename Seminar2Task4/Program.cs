0be8643462d30601ee7e0c46738dba231def7b0d
Selectel Git User <git-user@selectel.ru>
2023-09-08 00:41:33 +0600

    Seminar2Task4

---
+++ Seminar2Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача №14
+//Напишите программу, которая принимает на вход число и проверяет,
+// кратно ли оно одновременно 7 и 23.
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(num % 7 == 0 && num % 23 == 0 ? "Кратно" : "Не кратно");
+
+int GetNum()
+{
+    Console.Write("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = GetNum();
+
+Console.WriteLine(num % 7 == 0 && num % 23 == 0 ? "Кратное" : "Не кратно");
