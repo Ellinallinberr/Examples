7f94943c74ff4f35e4c709827d1cc086e08c0e4c
Selectel Git User <git-user@selectel.ru>
2023-09-08 00:20:26 +0600

    Seminar2Task2

---
+++ Seminar2Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача №11
+//Напишите программу, которая выводит случайное трёхзначное 
+//число и удаляет вторую цифру этого числа.
+
+// int num = new Random().Next(100,1000);
+
+// Console.WriteLine(num);
+
+// int firstDigin = num/100;
+// int lastDigit = num%10;
+// num = firstDigin*10 + lastDigit;
+
+// Console.WriteLine(num);
+
+int ran = new System.Random().Next(100, 1000);
+    System.Console.WriteLine(ran);
+    char[] digitChar = ran.ToString().ToCharArray();
+    System.Console.Write(digitChar[0]);
+    System.Console.Write(digitChar[2]);
+
+    
