299b9c72350cb770fe4f82daa9f72cd9a6e79991
Selectel Git User <git-user@selectel.ru>
2023-09-08 00:09:04 +0600

    Seminar2Task1

---
+++ Seminar2Task1/Program.cs
@@ -0,0 +1,51 @@
+﻿//Задача №9 
+//Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+//и показывает наибольшую цифру числа.
+void Variant1()
+{
+Console.WriteLine("Cпособ 1");
+
+System.Random numberGenerator = new System.Random();
+int number = numberGenerator.Next(10, 100);
+Console.WriteLine(number);
+
+int firstDigit =number/10;
+int secondDigit =number%10;
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else
+{
+    Console.WriteLine(secondDigit);
+}
+}
+void Variant2()
+{
+Console.WriteLine("Способ 2");
+
+    System.Random numberGenerator1 = new System.Random();
+    int number1 = numberGenerator1.Next(10, 100);
+    Console.WriteLine(number1);
+
+    Console.WriteLine((number1 / 10 > number1 % 10) ? (int)(number1 / 10) : (int)(number1 % 10));
+}
+void Variant3()
+{
+Console.WriteLine("Способ 3");
+
+    System.Random numberGenerator = new System.Random();
+    int number = numberGenerator.Next(10, 100);
+    Console.WriteLine(number);
+
+    char[] digitChar = number.ToString().ToCharArray();
+    Console.WriteLine((int)digitChar[0]>(int)digitChar[1]?digitChar[0]:digitChar[1]);
+    //<условие>?<вариант1>:<вариант2>
+
+}
+
+//Variant1();
+//Variant2();
+Variant3();
+
