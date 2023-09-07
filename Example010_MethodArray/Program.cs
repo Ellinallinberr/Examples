4a65bcf9b1a65f6f723655641613211001773619
Selectel Git User <git-user@selectel.ru>
2023-09-07 22:48:35 +0600

    Example010_MethodArray/

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1,2,3,46,33,24,25,56};
+
+int n = array.Length;
+int find = 24;
+
+int index = 0;
+
+while (index < n) 
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //прерви(если несколько одинаковых чисел)
+    }
+    index++;
+}
