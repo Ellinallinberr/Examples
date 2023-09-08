9aa79fb918c07a64f3a9239b8cc62c540fdf7544
Selectel Git User <git-user@selectel.ru>
2023-09-09 01:56:55 +0600

    HomeWorkTask2

---
+++ HomeWorkTask2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numA = int.Parse(Console.ReadLine());//??"0"
+Console.WriteLine("Введите второе число: ");
+int numB = int.Parse(Console.ReadLine());//??"0"
+
+if (numA >numB)
+{
+    Console.WriteLine("Первое число больше");
+}
+else 
+{
+    Console.WriteLine("Второе число больше");
+}
