c5c89228649ace3b9238b6988a076e48d72b2689
Selectel Git User <git-user@selectel.ru>
2023-09-09 22:48:28 +0600

    HomeWorkTask4

--- HomeWorkTask4/Program.cs
+++ HomeWorkTask4/Program.cs
@@ -10,3 +10,5 @@ if (numB> numMax) numMax = numB;
 if (numC> numMax) numMax = numC;
 
 Console.WriteLine ("Число "+ numMax +" больше других чисел");
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
