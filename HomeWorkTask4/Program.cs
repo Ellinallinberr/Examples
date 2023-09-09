1408eb0654253185f78105d96babfd809e50d7da
Selectel Git User <git-user@selectel.ru>
2023-09-09 22:45:46 +0600

    HomeWorkTask4

--- HomeWorkTask4/Program.cs
+++ HomeWorkTask4/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите три числа: ");
+int numA = int.Parse(Console.ReadLine());//??"0"
+Console.WriteLine(". ");
+int numB = int.Parse(Console.ReadLine());//??"0"
+Console.WriteLine(". ");
+int numC = int.Parse(Console.ReadLine());//??"0"
+
+int numMax = numA;
+if (numB> numMax) numMax = numB;
+if (numC> numMax) numMax = numC;
+
+Console.WriteLine ("Число "+ numMax +" больше других чисел");
