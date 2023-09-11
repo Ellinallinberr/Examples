754ad4f1d41a7c352208ac31901f5a384d250fdd
Selectel Git User <git-user@selectel.ru>
2023-09-11 21:11:07 +0600

    HomeWorkTask13and15

---
+++ HomeWorkTask13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+string num = Console.ReadLine();
+
+if(int.Parse(num) < 99) 
+{
+    Console.WriteLine("Третьей цифры нет");
+    return;
+}
+
+Console.WriteLine(num[2]);
--- Seminar1Task2/Program.cs
+++ Seminar1Task2/Program.cs
@@ -3,12 +3,12 @@ string day = Console.ReadLine()??"0";
 
 switch (day)
 {
-    case "1": Console.WriteLine("Понедельник"); break;
-    case "2": Console.WriteLine("Вторник"); break;
-    case "3": Console.WriteLine("Среда"); break;
-    case "4": Console.WriteLine("Четверг"); break;
-    case "5": Console.WriteLine("Пятница"); break;
-    case "6": Console.WriteLine("Суббота"); break;
-    case "7": Console.WriteLine("Воскресенье"); break;
+    case "1": Console.WriteLine("нет"); break;
+    case "2": Console.WriteLine("нет"); break;
+    case "3": Console.WriteLine("нет"); break;
+    case "4": Console.WriteLine("нет"); break;
+    case "5": Console.WriteLine("нет"); break;
+    case "6": Console.WriteLine("да"); break;
+    case "7": Console.WriteLine("да"); break;
     default: Console.WriteLine("Неверные данные"); break;
 }
