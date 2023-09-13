3fa90442e7d368adabb750594aa9ac6ce60fdf13
Selectel Git User <git-user@selectel.ru>
2023-09-14 01:03:12 +0600

    Seminar3Task9

---
+++ Seminar3Task9/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 22: Напишите программу, которая
+//принимает на вход число (N) и выдаёт таблицу
+//квадратов чисел от 1 до N.
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+string BuildLine(int num,int pow)
+{
+    string res = String.Empty;
+    for (int i = 1;i<=num;i++)
+    {
+        res = res + Math.Pow(i,pow) + " ";
+    }
+
+    return res;
+}
+
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+int Num = ReadData("Enter N");
+int pow = ReadData("Enter pow");
+Console.WriteLine("Квадраты числа N: "+BuildLine(Num,pow));
