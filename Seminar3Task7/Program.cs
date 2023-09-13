915be36a85f3013168d27dba945749fdc1c7aff3
Selectel Git User <git-user@selectel.ru>
2023-09-14 00:28:09 +0600

    Seminar3Task

--- Seminar3Task7/Program.cs
+++ Seminar3Task7/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 18: Напишите программу, которая по
+//заданному номеру четверти, показывает
+//диапазон возможных координат точек в этой
+//четверти (x и y).
+
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return (num);
+}
+
+string QuterBorderAsk(int numQut)
+{
+    if (numQut == 1) return "x>0,y>0";
+    if (numQut == 2) return "x>0,y<0";
+    if (numQut == 3) return "x<0,y<0";
+    if (numQut == 4) return "x<0,y>0";
+    return "empty";
+}
+
+
+void PrintResult(string msg) // печатаем результат
+{
+    Console.WriteLine("Координаты находятся в диапазоне следующих значений: "+ msg);
+}
+int quater = ReadData("Введите название четверти");
+PrintResult(QuterBorderAsk(quater));
