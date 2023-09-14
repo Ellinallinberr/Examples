61b2923208225e3d0cbd1a35ce297aa3336fb3dd
Selectel Git User <git-user@selectel.ru>
2023-09-14 23:42:20 +0600

    Seminar4

---
+++ Seminar4Task10/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 24: Напишите программу, которая
+//принимает на вход число (А) и выдаёт сумму чисел
+//от 1 до А.
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+Console.WriteLine("Введите число: ");
+
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
