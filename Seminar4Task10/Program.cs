51b505ac533bd9e86961c0d0072af1cf85fbd436
Selectel Git User <git-user@selectel.ru>
2023-09-20 14:27:14 +0600

    Seminar4Task10

--- Seminar4Task10/Program.cs
+++ Seminar4Task10/Program.cs
@@ -8,9 +8,27 @@ int ReadData(string msg)
     return num;
 }
 
-Console.WriteLine("Введите число: ");
-
 void PrintResult(string msg)
 {
     Console.WriteLine(msg);
 }
+//1.Вводим чило пользователя
+//2. вычисляем сумму
+//3. выводим результат
+
+
+int SimpleSum(int numA)
+{
+    int sumOfNumber = 0;
+    for (int i = 0; i < numA; i++)
+    {
+        sumOfNumber = sumOfNumber + i;
+        //sumOfNumber+=i;
+    }
+    return sumOfNumber;
+}
+
+int numberA = ReadData("Введите число А: ");
+
+int res = SimpleSum(numberA);
+PrintResult("Сумма чисел от 1 до " + numberA + " равна " + res);
