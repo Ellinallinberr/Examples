ae5576c401d09877bcd64a994f0a1004cd524fab
Selectel Git User <git-user@selectel.ru>
2023-09-20 23:37:21 +0600

    HomeWorkTask27

--- HomeWorkTask27/Program.cs
+++ HomeWorkTask27/Program.cs
@@ -23,3 +23,53 @@ int SumDigit(int num)
 }
 int num = ReadData("Введите число: ");
 Console.WriteLine("Сумма чисел в числе " + num + " равна " + SumDigit(num));
+
+// //                                                 Тело программы
+// //------------------------------------------------------------------------------------------------------------------
+// //Console.Clear(); // Очистка консоли
+// int number = ReadData("Введите целое число: "); // Ввод числа с клавиатуры
+
+// CalculateAndPrintSum("Метод деления", number, SumDigitDiv); // Сумма методом деления
+// //CalculateAndPrintSum("Метод строки", number, SumDigitStr);  // Сумма методом строки
+
+
+// //                                                     Методы
+// //------------------------------------------------------------------------------------------------------------------
+// // Ввод данных с клавиатуры
+// int ReadData(string msg)
+// {
+//     Console.Write(msg);
+//     int num = int.Parse(Console.ReadLine() ?? "0");
+//     return num;
+// }
+
+// // Вывод результата
+// void CalculateAndPrintSum(string methodName, int number, Func<int, int> calculator)
+// {
+//     DateTime startTime = DateTime.Now;
+//     int sum = calculator(number);
+//     TimeSpan executionTime = DateTime.Now - startTime;
+//     Console.WriteLine($"\n{methodName}:\nСумма цифр числа {number} = {sum}\nВремя исполнения {executionTime}");
+// }
+
+// // Вычисление суммы цифр через вещественные числа
+// int SumDigitDiv(int number)
+// {
+//     int result = 0;
+//     while (number > 0)
+//     {
+//         result += number % 10;
+//         number /= 10;
+//     }
+//     return result;
+// }
+
+// //Вычисление суммы цифр через строку
+// int SumDigitStr(int number)
+// {
+//     string numberStr = number.ToString();
+//     int result = 0;
+
+//     foreach (char digitChar in numberStr) { result += int.Parse(digitChar.ToString()); }
+//     return result;
+// }
