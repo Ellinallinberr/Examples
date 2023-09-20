ee40f7340237825a38848eebe3cac7e8c3d1487d
Selectel Git User <git-user@selectel.ru>
2023-09-20 20:16:56 +0600

    HomeWorkTask11

---
+++ Seminar4Task11/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 26: Напишите программу, которая принимает
+//на вход число и выдаёт количество цифр в числе.
+
+
+int ReadData(string msg) // вводим данные
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+int numLength(int n)
+{
+    return Convert.ToInt32(Math.Log(n,10)+1);
+}
+
+int n = ReadData("Введите число");
+Console.WriteLine("количество цифр в числе = " + numLength(n));
