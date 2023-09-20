216e428895a729cad4cb6b9a782be5bd7f77d5df
Selectel Git User <git-user@selectel.ru>
2023-09-20 23:25:59 +0600

    HomeWorkTask27

---
+++ HomeWorkTask27/Program.cs
@@ -0,0 +1,11 @@
+﻿// №27 Напишите программу, которая принимает на 
+//вход число и выдаёт сумму цифр в числе.
+
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
