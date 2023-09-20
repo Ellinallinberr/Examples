bde3c6f68f6cbdf2b7c1df77b7bef457d30a77da
Selectel Git User <git-user@selectel.ru>
2023-09-20 23:25:47 +0600

    HomeWorkTask25

--- HomeWorkTask25/Program.cs
+++ HomeWorkTask25/Program.cs
@@ -26,3 +26,85 @@ long MyPow(int A, int B)
 int A = ReadData("Введите первое число: ");
 int B = ReadData("Введите степень: ");
 Console.WriteLine("Число "+ A+" в степени "+ B +" = "+ MyPow(A,B));
+
+// //Вводим данные
+// string ReadData(string msg)
+// {
+//     Console.WriteLine(msg);
+//     string data = Console.ReadLine();
+//     return data;
+// }
+
+// //Функция сложения
+// double plus(int a, int b)
+// {
+//     double result = a + b;
+//     return result;
+// }
+
+// //Функция вычитания
+// double minus(int a, int b)
+// {
+//     double result = a - b;
+//     return result;
+// }
+
+// //Функция умножения
+// double multiplication(int a, int b)
+// {
+//     double result = a * b;
+//     return result;
+// }
+
+// //Функция деления
+// double division(int a, int b)
+// {
+//     double result = a / b;
+//     return result;
+// }
+
+// //Функция возведения в степень
+// double degree(int a, int b)
+// {
+//     double result = Math.Pow(a, b);
+//     return result;
+// }
+
+// //Печатаем результат
+// void PrintResult(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+
+
+
+// int a = int.Parse(ReadData("Введите число А"));
+// int b = int.Parse(ReadData("Введите число B"));
+// string operand = ReadData("Введите операцию (+, -, *, /, ^)");
+
+// //double result = getResult(a, b, operand);
+
+// double result = 0;
+
+// switch (operand)
+// {
+//     case "+": 
+//     result = plus(a, b);
+//     break;
+//     case "-": 
+//     result = minus(a, b);
+//     break;
+//     case "*": 
+//     result = multiplication(a, b);
+//     break;
+//     case "/": 
+//     result = division(a, b);
+//     break;
+//     case "^": 
+//     result = degree(a, b);
+//     break;
+//     default: Console.WriteLine("Что-то пошло не так");
+//     break;
+// }
+
+// PrintResult("Результат операции = " + result);
