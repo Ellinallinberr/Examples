c9d6ca4a169430a4ed91e21727bf98bdb209356b
Selectel Git User <git-user@selectel.ru>
2023-09-20 18:40:12 +0600

    HomeWorkTask23

---
+++ HomeWorkTask23/Program.cs
@@ -0,0 +1,37 @@
+﻿//Внутри класса Answer напишите метод ShowCube,
+// который принимает на вход число (N) и выводит 
+//таблицу кубов чисел от 1 до N (включительно) каждое на новой строке.
+
+int topNum = ReadData("Введите целое число больше 0: ");
+
+while (topNum < 1)
+{
+    Console.WriteLine("Число должно быть больше 0!");
+    topNum = ReadData("Введите целое число боьше 0: ");
+}
+int i = 1; // счетчик для цикла
+int cube = 0; //куб числа
+int numDigitCube = 0; //количество цифр в кубе числа
+string firstStr = "\u2551 Число:   \u2551"; // первая строка таблицы с границами ввиде псевдографики
+string secStr = "\u2551 Куб числа:   \u2551";// вторая
+
+while (i <= topNum)
+{
+    cube = (int)Math.Pow(i, 3); //вычисляем куб числа
+    numDigitCube =(int)Math.Log10(cube) +1;// вычисляем кол-во цифр в кубе числа через логорифм
+    firstStr = firstStr+((i).ToString()).PadRight(numDigitCube)+"\u2551";
+    secStr = secStr+cube.ToString()+ "\u2551";
+    i++;
+}
+
+DrawUniLine(firstStr,"\u2554","\u2566","\u2557");
+Console.WriteLine(firstStr);
+DrawUniLine(firstStr,"\u2560","\u256C","\u2563");
+Console.WriteLine(secStr);
+DrawUniLine(firstStr,"\u255A","\u2569","\u255D");
+
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
---
+++ HomeWorkTask3D/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Answer
+{
+    private static double Length(int[] pointA, int[] pointB)
+    {
+     int x1, x2, x3, y1, y2, y3;
+      x1 = pointA[0];
+      x2 = pointA[1];
+      x3 = pointA[2];
+      y1 = pointB[0];
+      y2 = pointB[1];
+      y3 = pointB[2];
+      return Math.Round(Math.Sqrt((Math.Pow(Math.Abs(y1-x1), 2)) + (Math.Pow(Math.Abs(y2 -x2), 2)) + (Math.Pow(Math.Abs(y3-x3), 2))), 2);
+    }
+
+  // Не удаляйте и не меняйте метод Main! 
+    public static void Main(string[] args) {
+        int x1, x2, x3, y1, y2, y3;
+
+        if (args.Length >= 6) {
+            x1 = int.Parse(args[0]);
+            x2 = int.Parse(args[1]);
+            x3 = int.Parse(args[2]);
+            y1 = int.Parse(args[3]);
+            y2 = int.Parse(args[4]);
+            y3 = int.Parse(args[5]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            x1 = 3;
+            x2 = 6;
+            x3 = 8;
+            y1 = 2;
+            y2 = 1;
+            y3 = -7;
+        }
+
+        // Не удаляйте строки ниже
+        double result = Length(new int[]{x1, x2, x3}, new int[]{y1, y2, y3});
+        Console.WriteLine($"{result:F2}");
+    }
+}
