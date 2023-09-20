40170d37e2feedd5478fa1c1f0e45ad2e94dfa9d
Selectel Git User <git-user@selectel.ru>
2023-09-20 20:24:02 +0600

    HomeWorkTask12

---
+++ Seminar4Task12/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 28: Напишите программу, которая
+//принимает на вход число N и выдаёт
+//произведение чисел от 1 до N.
+
+using System.Numerics;
+
+// Вводим данные
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+// Выводим результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+// Считаем цифры
+BigInteger CountFact(int num)
+{
+    BigInteger fact = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
+
+int num = ReadData("Введите целое число: ");
+BigInteger fact = CountFact(num);
+PrintResult("Факториал числа " + num + " равен " + fact);
