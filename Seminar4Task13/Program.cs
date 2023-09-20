182fe616c74f21d464c8538d16eec6e2610ea41a
Selectel Git User <git-user@selectel.ru>
2023-09-20 22:28:01 +0600

    Seminar4Task13

---
+++ Seminar4Task13/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача 30: Напишите программу, которая
+//выводит массив из 8 элементов, заполненный
+//нулями и единицами в случайном порядке.
+//[1,0,1,1,0,1,0,0]
+
+// Вводим данные
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+// Печатаем результат
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+        
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+// Вычисляем факториал
+int[] GenArr(int len)
+{
+    int[] arr = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = rnd.Next(0, 2);
+    }
+    return arr;
+}
+
+int num = ReadData("Введите число: ");
+int[] res = GenArr(num);
+PrintArray(res);
