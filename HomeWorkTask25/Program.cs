ffce0f81b225752cb14fa8295e9b0379dd8fc68c
Selectel Git User <git-user@selectel.ru>
2023-09-20 23:21:41 +0600

    HomeWorkTask25

---
+++ HomeWorkTask25/Program.cs
@@ -0,0 +1,28 @@
+﻿//№25 Напишите цикл, который принимает на 
+//вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+long MyPow(int A, int B)
+{
+    long res = 1;
+    while (B >= 0)
+    {
+        res = res * A;
+        B = B - 1;
+    }
+    return res;
+    
+}
+
+
+//A = A*
+
+int A = ReadData("Введите первое число: ");
+int B = ReadData("Введите степень: ");
+Console.WriteLine("Число "+ A+" в степени "+ B +" = "+ MyPow(A,B));
