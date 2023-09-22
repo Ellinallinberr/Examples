c02934a7c89018a69f5eabf0196ddd30626753fe
Selectel Git User <git-user@selectel.ru>
2023-09-22 21:53:58 +0600

    HomeWorkTask34

---
+++ HomeWorkTask34/Program.cs
@@ -0,0 +1,57 @@
+﻿// №34 Задайте массив заполненный случайными положительными 
+//трёхзначными числами. Напишите программу, которая покажет 
+//количество чётных чисел в массиве.
+
+
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+// Находим количество элементов в заданном диапазоне
+int CountElm(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]%2==0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+//передаем длину массива и генерируем нужное количество чисел
+int[] GenerateThreeDigitNumbersArray(int length)
+{
+    Random random = new Random();
+    int[] numbersArray = new int[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        int randomNumber = random.Next(100, 1000);
+        numbersArray[i] = randomNumber;
+    }
+
+    return numbersArray;
+}
+
+//Печатаем результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+Console.WriteLine("Введите число: ");
+string length = Console.ReadLine(); 
+int[] result = GenerateThreeDigitNumbersArray(int.Parse(length));
+int count = CountElm(result);
+PrintArray(result);
+PrintResult($"{count} четных элементов");
