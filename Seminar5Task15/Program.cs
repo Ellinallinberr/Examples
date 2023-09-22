1b1603eeff03d6a06e63d7f6b6b53f27e22bc8f9
Selectel Git User <git-user@selectel.ru>
2023-09-22 15:14:16 +0600

    Seminar5Task15

---
+++ Seminar5Task15/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 32: Напишите программу замена элементов
+//массива: положительные элементы замените на
+//соответствующие отрицательные, и наоборот.
+
+int[] GenArray(int length, int lowBorder, int highBorder)
+{
+    int[] arr = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next(lowBorder, highBorder + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write($"[ ");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.Write(arr[arr.Length - 1] + " ]");
+    System.Console.WriteLine();
+}
+
+void InversArray(int[] arr)
+{
+    for(int i=0;i<arr.Length;i++)
+    {
+        arr[i]*=-1;
+    }
+}
+
+int[] array=GenArray(12,-9,9);
+PrintArray(array);
+InversArray(array);
+PrintArray(array);
