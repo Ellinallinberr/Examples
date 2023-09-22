88eeda2e7f499e57bfca746cabb0737fe3c6b00a
Selectel Git User <git-user@selectel.ru>
2023-09-22 18:39:25 +0600

    Seminar5Task16

---
+++ Seminar5Task16/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 33: Задайте массив. Напишите программу, которая
+//определяет, присутствует ли заданное число в массиве.
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine()??",";
+    int number = Convert.ToInt32(value);
+
+    return number;
+}
+
+// Проверяет, есть ли число
+string Check(int[] array, int num)
+{
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] == num)
+            return $"Есть, индекс {i}";
+    return "Нет";
+}
+
+// Выводит элементы массива в консоль
+void OutputArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+        Console.Write(array[i] + ", ");
+    Console.WriteLine(array[array.Length - 1] + "]");
+}
+
+
+int[] array = {1, -2, 5, 6, 58, 7, -3, -42, 63, 15};
+OutputArray(array);
+
+int element = Prompt("Введите число для поиска: ");
+
+Console.WriteLine(Check(array, element));
