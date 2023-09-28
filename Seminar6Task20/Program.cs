13ae99404ccf0265fb6af88668030b1bc8ea4658
Selectel Git User <git-user@selectel.ru>
2023-09-28 16:17:41 +0600

    Seminar6Task20

--- Seminar6Task20/Program.cs
+++ Seminar6Task20/Program.cs
@@ -1,3 +1,15 @@
 ﻿//Задача 40: Напишите программу, которая принимает на вход три числа и
 //проверяет, может ли существовать треугольник с сторонами такой длины.
 
+double ReadData(){
+    return double.Parse(Console.ReadLine()??"0");
+}
+
+
+bool isTrianglable(double a, double b, double c)
+{
+    return !(a+b <= c || b+c <= a || a+c <= b);
+}
+
+
+Console.WriteLine(isTrianglable(ReadData(),ReadData(),ReadData()));
