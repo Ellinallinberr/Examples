e4285c602ea4acff31bbf3ccf4ff4b633e2fcbde
Selectel Git User <git-user@selectel.ru>
2023-10-01 19:34:20 +0600

    Seminar7Task26/

---
+++ Seminar7Task26/Program.cs
@@ -0,0 +1,58 @@
+﻿//Задача 49: Задайте двумерный массив. Найдите элементы, у
+//которых оба индекса чётные, и замените эти элементы на их
+//квадраты.
+
+
+int ReadData(string msg) // вводим данные
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+
+int[,] Fill2DArray(int countRow, int countColumn, int topBorder, int downBorder)
+{
+    int[,] array2D = new int[countRow, countColumn];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i, j] = new Random().Next(downBorder, topBorder + 1);
+        }
+    }
+    return array2D;
+}
+
+void Print2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(matrix[i,j]+" ");
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+}
+
+void EvenToSquare(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(i%2 ==  0 && j%2 == 0) matrix[i,j] = matrix[i,j]*matrix[i,j];
+        }
+        
+    }
+}
+
+int row = ReadData("введите количество строк: ");
+int column = ReadData("Введите количество столбцов: ");
+int[,] arr2D = Fill2DArray(row,column,10,1);
+Print2DArray(arr2D);
+EvenToSquare(arr2D);
+Print2DArray(arr2D);
