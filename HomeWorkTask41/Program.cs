feee7bb5957dfb0b73656855ada2100fdc34cd5f
Selectel Git User <git-user@selectel.ru>
2023-10-01 19:07:07 +0600

    HomeWorkTask41

---
+++ HomeWorkTask41/Program.cs
@@ -0,0 +1,62 @@
+﻿﻿//------------------------------------------------------------------------------------------------------------------
+//                                                  Задача 41
+//                                          Домашнее задание к семинару 06
+//   Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//   (*) Пользователь вводит число нажатий, затем программа следит за нажатиями и выдает сколько чисел больше 0 было введено.
+//------------------------------------------------------------------------------------------------------------------
+
+//                                                 Тело программы
+//------------------------------------------------------------------------------------------------------------------
+using System.Text.RegularExpressions;
+Console.Clear();
+
+int numOfKeyPresses = ReadInt("Введите количество нажатий: ");  // Ввод пользователем количества нажатий.
+string data = KeyPresses("Введите строку: ", numOfKeyPresses);  // Получение нажатий с клавиатуры.
+//Console.WriteLine(data);
+int count = CountPositiveNumbers(data);                         // Подсчет положительных чисел в строке.
+PrintResult($"\nВведено чисел больше нуля: {count}");           // Вывод результата в консоль.
+
+//                                                     Методы
+//------------------------------------------------------------------------------------------------------------------
+
+// Ввод целого числа с клавиатуры.
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Получение нажатий с клавиатуры.
+string KeyPresses(string msg, int numOfKeyPresses)
+{
+    Console.Write(msg);
+    string data = string.Empty;
+    
+    for (int i = 0; i < numOfKeyPresses; i++)
+    {
+        var keyInfo = Console.ReadKey();
+        data += keyInfo.KeyChar;
+    }
+
+    return data;
+}
+
+// Подсчет положительных чисел в строке с использованием регулярных выражений.
+int CountPositiveNumbers(string input)
+{
+    string pattern = @"[-]?[0-9]+";
+    MatchCollection matches = Regex.Matches(input, pattern);
+
+    int count = 0;
+    foreach (Match match in matches)
+    {
+        if (int.Parse(match.Value) > 0) { count++; }
+    }
+    return count;
+}
+
+// Вывод результата в консоль.
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
