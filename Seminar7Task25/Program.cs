688bd02d25b9231832bc459f7a1f3464dc1eeb57
Selectel Git User <git-user@selectel.ru>
2023-10-01 19:31:07 +0600

    HomeWorkTask48

---
+++ Seminar7Task25/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача 48: Задайте двумерный массив размера m на n,
+//каждый элемент в массиве находится по формуле: Aₘₙ = m+n.
+//Выведите полученный массив на экран.
+
+﻿// Вывод сообщения и запись введённых данных
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine()??",";
+    int number = Convert.ToInt32(value);
+
+    return number;
+}
+// Заполняет массив
+void FillingArray(int[,] array, int m, int n)
+{
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            array[i, j] = i + j;
+}
+// Выводит элементы массива в консоль
+void OutputArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("{");
+
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+            Console.Write(array[i, j] + ",\t ");
+
+        Console.WriteLine(array[i, array.GetLength(1) - 1] + "}");
+    }
+}
+
+int m = Prompt("Введите m: ");
+int n = Prompt("Введите n: ");
+
+int[,] array = new int[m, n];
+FillingArray(array, m, n);
+OutputArray(array);
