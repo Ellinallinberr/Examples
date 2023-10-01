635d05dc84294265ed474561b0d02ac7c307d47f
Selectel Git User <git-user@selectel.ru>
2023-10-01 23:00:22 +0600

    HomeWorkTask47

---
+++ HomeWorkTask47/Program.cs
@@ -0,0 +1,73 @@
+﻿//Задача 47: Задайте двумерный массив размером m×n,
+//заполненный случайными вещественными числами.
+
+using System;
+
+public class Answer {
+  static Random random = new Random();
+
+  public static double[, ] CreateRandomMatrix(int m, int n, int minLimitRandom, int maxLimitRandom) {
+    double[, ] matrix = new double[m, n];
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        matrix[i, j] = random.Next(minLimitRandom, maxLimitRandom + 1) + random.NextDouble();
+      }
+    }
+
+    return matrix;
+  }
+
+  public static void PrintArray(double[, ] matrix) {
+    for (int i = 0; i < matrix.GetLength(0); i++) {
+      for (int j = 0; j < matrix.GetLength(1); j++) {
+        Console.Write($"{matrix[i, j]:f2}\t");
+      }
+      Console.WriteLine();
+    }
+  }
+
+  public static void Main(string[] args) {
+    int m, n, minLimitRandom, maxLimitRandom;
+
+    if (args.Length >= 4) {
+      m = int.Parse(args[0]);
+      n = int.Parse(args[1]);
+      minLimitRandom = int.Parse(args[2]);
+      maxLimitRandom = int.Parse(args[3]);
+
+      double[,] array = CreateRandomMatrix(m, n, minLimitRandom, maxLimitRandom);
+
+        // Выберем случайные индексы трех элементов матрицы array
+        int row1 = new Random().Next(0, m);
+        int col1 = new Random().Next(0, n);
+        int row2 = new Random().Next(0, m);
+        int col2 = new Random().Next(0, n);
+        int row3 = new Random().Next(0, m);
+        int col3 = new Random().Next(0, n);
+
+        // Проверяем, являются ли выбранные элементы дробными числами
+        bool isFractional1 = (array[row1, col1] % 1) != 0;
+        bool isFractional2 = (array[row2, col2] % 1) != 0;
+        bool isFractional3 = (array[row3, col3] % 1) != 0;
+
+        // Если два из трех элементов не являются дробными числами, то бросаем исключение
+        if ((isFractional1 && isFractional2) || (isFractional1 && isFractional3) || (isFractional2 && isFractional3))
+        {
+            Console.WriteLine("Все ок!");
+        }
+        else
+        {
+            throw new Exception("Выбранные элементы не содержат по крайней мере два дробных числа.");
+        }
+    } else {
+      m = 3;
+      n = 4;
+      minLimitRandom = -10;
+      maxLimitRandom = 10;
+
+      double[, ] result = CreateRandomMatrix(m, n, minLimitRandom, maxLimitRandom);
+      PrintArray(result);
+    }
+  }
+}
---
+++ Seminar7Task27/Program.cs
@@ -0,0 +1,57 @@
+﻿//Задача 51: Задайте двумерный массив. Найдите элементы, у
+//которых оба индекса чётные, и замените эти элементы на их
+//квадраты.
+
+﻿// Вывод сообщения и запись введённых данных
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine()??",";
+    int number = Convert.ToInt32(value);
+
+    return number;
+}
+// Заполняет массив случайными цифрами
+void Random2DArray(int[,] array, int minElement, int maxElement)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(minElement, maxElement);
+}
+// Ищет элементы, у которых оба индекса чётные и меняет их
+int MainDiagonalSum(int[,] array)
+{
+    int sum = 0;
+
+    for (int i = 0; i < array.GetLength(0) && i < array.GetLength(1); i++)
+        sum += array[i, i];
+
+    return sum;
+}
+
+// Выводит элементы массива в консоль
+void Output2DArray(int[,] array, string message)
+{
+    Console.WriteLine(message);
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("{");
+
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+            Console.Write(array[i, j] + ",\t ");
+
+        Console.WriteLine(array[i, array.GetLength(1) - 1] + "}");
+    }
+}
+
+int m = Prompt("Введите m: ");
+int n = Prompt("Введите n: ");
+int minElement = 0;
+int maxElement = 10;
+
+int[,] array = new int[m, n];
+Random2DArray(array, minElement, maxElement);
+Output2DArray(array, "Массив: ");
+
+Console.WriteLine("Сумма эл-тов главной диагонали: " + MainDiagonalSum(array));
