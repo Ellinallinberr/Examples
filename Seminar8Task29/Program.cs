1bcc1a43066efdedb109af2df78f14b113a22cfd
Selectel Git User <git-user@selectel.ru>
2023-10-03 16:21:57 +0600

    Seminar8Task29

---
+++ Seminar8Task29/Program.cs
@@ -0,0 +1,57 @@
+﻿//Задача 55: Задайте двумерный массив. Напишите программу,
+//которая заменяет строки на столбцы. В случае, если это
+//невозможно, программа должна вывести сообщение для
+//пользователя.
+
+// Заполняем двумерный массив
+int[,] Fill2DArray(int countRow, int countColumn, int topBorder, int downBorder)
+{
+    System.Random rnd = new System.Random();
+    int[,] array2D = new int[countRow, countColumn];
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i, j] = rnd.Next(downBorder, topBorder);
+        }
+    }
+    return array2D;
+}
+
+// Распечатать массив
+void Print2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Rotate2DArray(int[,] matrix)
+{
+    int buf = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 1 + i; j < matrix.GetLength(1); j++)
+        {
+            buf = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = buf;
+        }
+    }
+}
+
+bool TestArrayRotate(int[,] matrix)
+{
+    if (matrix.GetLength(0) == matrix.GetLength(1)) return true;
+    else return false;
+}
+
+Console.Clear();
+int[,] arr2D = Fill2DArray(3, 3, 10, 1);
+Print2DArray(arr2D);
