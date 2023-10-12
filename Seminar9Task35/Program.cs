26b0b342c0725df7116ca9f8bbb5b5a4ccb17fe3
Selectel Git User <git-user@selectel.ru>
2023-10-12 22:51:06 +0600

    Seminar9Task36

---
+++ Seminar9Task35/Program.cs
@@ -0,0 +1,45 @@
+﻿//Задача 67. Напишите программу которая принимает число N  и на выходже выдает сумму цифр в числе.
+
+﻿// int ReadData(string msg) // вводим данные
+// {
+//     Console.WriteLine(msg);
+//     int num = int.Parse(Console.ReadLine() ?? "0");
+//     return num;
+// }
+
+// int digitSum(int num)
+// {
+
+//     if(num > 0)
+//     {
+//     return  digitSum(num/10)+num%10;
+//     } else return 0;
+
+// }
+
+// int num = ReadData("Введите число");
+// Console.WriteLine($"Сумма цифр { digitSum(num)}");
+
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+int SumDigitRec(int num)
+{
+
+    if (num == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return num % 10 + SumDigitRec(num / 10);
+    }
+
+}
+
+int number=ReadData($"Введите число: ");
+System.Console.WriteLine($"Сумма всех цифр в числе {number} равна {SumDigitRec(number)}.");
---
+++ Seminar9Task36/Program.cs
@@ -0,0 +1,52 @@
+﻿//Задача 69.
+//Напишите програм которая принимает на вход два число А и В.
+//и На выходе возводит число А в степень В. с помощью рекурсии
+
+﻿// Вывод сообщения и запись введённых данных
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine()??",";
+    int number = Convert.ToInt32(value);
+
+    return number;
+}
+// Считает сумму
+int Exponentiation(int number, int stepen)
+{
+    return stepen <= 0  ? 1 : number * Exponentiation(number, stepen - 1);
+}
+
+// Считает сумму
+int Exponentiation1(int number, int stepen)
+{
+    return stepen == 2 
+    ? number * number
+    : stepen <= 1 
+        ? number 
+        : stepen % 2 == 0 
+            ? Exponentiation1(number, stepen / 2) * Exponentiation1(number, stepen / 2)
+            : Exponentiation1(number, stepen / 2 + 1) * Exponentiation1(number, stepen / 2);
+}
+
+long MyPow(int a,int b)
+{
+    if(b==2)return a*a;
+    if(b==1)return a;
+
+    if(b%2==0)
+    {
+       return MyPow(a,b/2)*MyPow(a,b/2);
+    }
+    else
+    {
+       return MyPow(a,b/2)*MyPow(a,b/2+1);
+    }
+}
+
+
+int number = Prompt("Введите число: ");
+int stepen = Prompt("Введите степень: ");
+
+Console.WriteLine($"Число {number} в степени {stepen} = {Exponentiation(number, stepen)}");
+Console.WriteLine($"Число {number} в степени {stepen} = {MyPow(number, stepen)}");
